47127b543c56b57b29f7493d908a2ebec9e0328e
Brian Kropff <brian.kropff@gmail.com>
2022-06-26 18:08:59 +0000

    Add the authors to the output and print the quotes before their names.

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text,'lxml')
 
 quotes = soup.find_all('span', class_='text')
-print(quotes)
+authors = soup.find_all('small', class_='author')
+
+for i in range(0,len(quotes)):
+  print(quotes[i].text)
+  print(authors[i].text)
