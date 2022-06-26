4ad158b8c969eb62d47983516c88cff8c45269b9
Brian Kropff <brian.kropff@gmail.com>
2022-06-26 17:57:07 +0000

    Add the basic setup for web scraping and grabbed the quotes from webpage

--- main.py
+++ main.py
@@ -7,13 +7,3 @@ soup = BeautifulSoup(response.text,'lxml')
 
 quotes = soup.find_all('span', class_='text')
 print(quotes)
-# quotes = soup.find_all("span", class_="text")
-
-# authors = soup.find_all("small", class_="author")
-# tags = soup.find_all("div", class_="tags")
-# for i in range(0,len(quotes)):
-#     print(quotes[i].text)
-#     print(authors[i].text)
-#     quoteTags = tags[i].find_all('a',class_='tag')
-#     for quoteTag in quoteTags:
-#         print(quoteTag.text)
