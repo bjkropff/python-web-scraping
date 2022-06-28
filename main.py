57ab925f9ff54764fb033d188cd236117a70d07a
Brian Kropff <brian.kropff@gmail.com>
2022-06-28 01:22:17 +0000

    Scrap through multiple web pages for data, still presenting it nicely

--- main.py
+++ main.py
@@ -14,3 +14,23 @@ for item in items:
   itemPrice = item.find('h5').text
   print( '%s ) Price: %s, Item name: %s' % ( count, itemPrice, itemName))
   count += 1
+
+pages = soup.find('ul', class_='pagination')
+urls = []
+links = pages.find_all('a', class_='page-link')
+for link in links:
+  pageNum = int(link.text) if link.text.isdigit() else None
+  if pageNum != None:
+    x = link.get('href')
+    urls.append(x)
+
+for i in urls:
+  newUrl = url + i
+  response = requests.get(newUrl)
+  soup = BeautifulSoup(response.text,'lxml')
+  items = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+  for item in items:
+    itemName = item.find('h4', class_='card-title').text.strip('\n')
+    itemPrice = item.find('h5').text
+    print( '%s ) Price: %s, Item name: %s' % ( count, itemPrice, itemName))
+    count += 1
